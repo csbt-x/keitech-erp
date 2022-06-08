9980e16fec76605df995eb89c3ef86a5ad6f123f
csbt-x <csbt.rsa@gmail.com>
2022-06-08 14:08:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-erp-system-l4d3fp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-erp-system-l4d3fp)
---
+++ src/app/stores/enums/store-type.enum.ts
@@ -0,0 +1,8 @@
+export enum StoreType {
+  Department,
+  MARKETPLACE,
+  WHAREHOUSE,
+  Discount,
+  ECommerce,
+  Corporate,
+}
